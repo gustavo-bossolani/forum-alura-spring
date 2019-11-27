220333216fdb36b50f81ea90ce96a5557e944d08
gustavo-bossolani <gustavo.wyd@outlook.com>
2019-11-26 22:07:53 -0300

    initial commit

---
+++ src/test/java/com/alura/exercicio/forum/ForumApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alura.exercicio.forum;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ForumApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
